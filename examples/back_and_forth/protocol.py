95e925ff77c137c3f5941067942e2a42e83e4ce5
Francisco Meirelles Fleury <franmeifleury@gmail.com>
2026-04-23 22:36:49 +0200

    Fixing back_and_forth example

--- examples/back_and_forth/protocol.py
+++ examples/back_and_forth/protocol.py
@@ -15,11 +15,12 @@ class BackAndForthProtocol(IProtocol):
     _log: logging.Logger
     _in_original_point: bool
     _state: str
+    _current_wp: tuple[float, float, float]
 
     def initialize(self) -> None:
         self._log = logging.getLogger()
         self._state = "WAITING"
-        self._in_original_point = True
+        self._current_wp = FAR_POINT
 
         self.provider.schedule_timer("goto", self.provider.current_time() + WAITING_INTERVAL)
     def handle_timer(self, timer: str) -> None:
@@ -30,7 +31,7 @@ class BackAndForthProtocol(IProtocol):
             self._state = "READY"
             self.provider.send_communication_command(SendMessageCommand(
                 message=f"GOTO", 
-                destination=1
+                destination=0
             ))
             self._log.info("Ready to move.")
 
@@ -39,23 +40,21 @@ class BackAndForthProtocol(IProtocol):
         if message.startswith("GOTO") and self._state == "READY":
             self._log.info(f"Received goto message, moving to far point")
             self._state = "MOVING"
-            self.provider.send_mobility_command(GotoCoordsMobilityCommand(*FAR_POINT))
+            self.provider.send_mobility_command(GotoCoordsMobilityCommand(*self._current_wp))
 
     def handle_telemetry(self, telemetry: Telemetry) -> None:
         if self._state != "MOVING":
             return
 
-        current_wp = FAR_POINT if self._in_original_point else ORIGINAL_POINT
-
-        error = ((telemetry.current_position[0] - current_wp[0]) ** 2 +
-                 (telemetry.current_position[1] - current_wp[1]) ** 2 +
-                 (telemetry.current_position[2] - current_wp[2]) ** 2) ** 0.5
-        self._log.info(f"Current position: {telemetry.current_position}, current waypoint: {current_wp}, error: {error}")
+        error = ((telemetry.current_position[0] - self._current_wp[0]) ** 2 +
+                 (telemetry.current_position[1] - self._current_wp[1]) ** 2 +
+                 (telemetry.current_position[2] - self._current_wp[2]) ** 2) ** 0.5
+        self._log.info(f"Current position: {telemetry.current_position}, current waypoint: {self._current_wp}, error: {error}")
 
         if error < WP_TOLERANCE:
-            self._log.info(f"Reached waypoint {current_wp}, switching to waiting state")
+            self._log.info(f"Reached waypoint {self._current_wp}, switching to waiting state")
             self._state = "WAITING"
-            self._in_original_point = not self._in_original_point
+            self._current_wp = ORIGINAL_POINT if self._current_wp == FAR_POINT else FAR_POINT
             self.provider.schedule_timer("goto", self.provider.current_time() + WAITING_INTERVAL)
 
     def finish(self) -> None:
--- examples/back_and_forth/runner.py
+++ examples/back_and_forth/runner.py
@@ -1,17 +1,18 @@
 from gradys_embedded.runner.runner import EmbeddedRunner
 from gradys_embedded.runner.configuration import RunnerConfiguration
 
-from protocol import SimpleUAVProtocol
+from protocol import BackAndForthProtocol
 
 if __name__ == "__main__":
     runner_configuration = RunnerConfiguration(
-        node_id=1,
+        node_id=0,
         node_ip_dict={
-            1: "http://localhost:5000",
+            0: "http://localhost:5000",
         },
         uav_api_port=8000,
-        origin_gps_coordinates=(-15.840081, -47.926642, -0.016),
+        #origin_gps_coordinates=(-15.840081, -47.926642, -0.016),
+        origin_gps_coordinates=None,  # Use current UAV position as origin
         initial_position=(0, 0, 2)
     )
-    runner = EmbeddedRunner(runner_configuration, SimpleUAVProtocol)
+    runner = EmbeddedRunner(runner_configuration, BackAndForthProtocol)
     runner.start_api()
